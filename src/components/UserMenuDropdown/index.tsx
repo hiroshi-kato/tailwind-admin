207f9c7e2301ef77bc9d7a04b93e23ab289e1c8f
hiroshi-kato <hiroshikato1208@gmail.com>
2021-03-16 12:56:22 +0900

    トランジションを追加

--- src/components/UserMenuDropdown/index.tsx
+++ src/components/UserMenuDropdown/index.tsx
@@ -1,5 +1,5 @@
 import { VFC } from 'react';
-import { Menu } from '@headlessui/react';
+import { Menu, Transition } from '@headlessui/react';
 import cn from 'classnames';
 
 type MenuItem = {
@@ -17,52 +17,73 @@ const UserMenuDropdown: VFC<UserMenuDropdownProps> = ({
   menuItems,
 }) => (
   <Menu>
-    <Menu.Button>
-      <span className="sr-only">ユーザーメニューを開く</span>
-      <img
-        className={cn('h-10', 'w-10', 'rounded-full', 'shadow-xl')}
-        src={iconUrl}
-        alt=""
-      />
-    </Menu.Button>
-    <Menu.Items
-      role="menu"
-      aria-orientation="vertical"
-      aria-labelledby="ユーザーメニュー"
-      className={cn(
-        'rounded-md',
-        'mt-2',
-        'w-48',
-        'shadow-lg',
-        'bg-white',
-        'ring-1',
-        'ring-black',
-        'ring-opacity-5',
-        'p-1',
-        'focus:outline-none',
-      )}
-    >
-      {menuItems.map((menuItem) => (
-        <Menu.Item>
-          {({ active }) => (
-            <button
-              type="button"
-              role="menuitem"
-              className={cn('w-full', 'text-left')}
-              onClick={menuItem.onClick}
-            >
-              <div
-                className={cn('py-2', 'px-4', 'text-sm', 'text-gray-700', {
-                  'bg-gray-100': active,
-                })}
-              >
-                {menuItem.content}
-              </div>
-            </button>
-          )}
-        </Menu.Item>
-      ))}
-    </Menu.Items>
+    {({ open }) => (
+      <>
+        <Menu.Button>
+          <span className="sr-only">ユーザーメニューを開く</span>
+          <img
+            className={cn('h-10', 'w-10', 'rounded-full', 'shadow-xl')}
+            src={iconUrl}
+            alt=""
+          />
+        </Menu.Button>
+        <Transition
+          show={open}
+          enter="transition duration-100 ease-out"
+          enterFrom="transform scale-95 opacity-0"
+          enterTo="transform scale-100 opacity-100"
+          leave="transition duration-75 ease-out"
+          leaveFrom="transform scale-100 opacity-100"
+          leaveTo="transform scale-95 opacity-0"
+        >
+          <Menu.Items
+            static
+            role="menu"
+            aria-orientation="vertical"
+            aria-labelledby="ユーザーメニュー"
+            className={cn(
+              'rounded-md',
+              'mt-2',
+              'w-48',
+              'shadow-lg',
+              'bg-white',
+              'ring-1',
+              'ring-black',
+              'ring-opacity-5',
+              'p-1',
+              'focus:outline-none',
+            )}
+          >
+            {menuItems.map((menuItem) => (
+              <Menu.Item>
+                {({ active }) => (
+                  <button
+                    type="button"
+                    role="menuitem"
+                    className={cn('w-full', 'text-left')}
+                    onClick={menuItem.onClick}
+                  >
+                    <div
+                      className={cn(
+                        'py-2',
+                        'px-4',
+                        'text-sm',
+                        'text-gray-700',
+                        {
+                          'bg-gray-100': active,
+                        },
+                      )}
+                    >
+                      {menuItem.content}
+                    </div>
+                  </button>
+                )}
+              </Menu.Item>
+            ))}
+          </Menu.Items>
+        </Transition>
+      </>
+    )}
   </Menu>
 );
 

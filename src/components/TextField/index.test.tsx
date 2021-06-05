091534c6c5f821069a26b4ca6bfd76e461b02add
mamedaifuku <hiroshikato1208@gmail.com>
2021-06-05 23:16:37 +0900

    TextFieldのテストエラーを修正

--- src/components/TextField/index.test.tsx
+++ src/components/TextField/index.test.tsx
@@ -1,17 +1,28 @@
-import { screen, render, cleanup } from 'test-utils';
+import { useForm } from 'react-hook-form';
+import { VFC } from 'react';
 
+import { screen, render, cleanup } from 'test-utils';
 import TextField, { TextFieldProps } from './index';
 
 afterEach(cleanup);
 
 describe('TextField', () => {
-  const defaultProps: TextFieldProps = {
-    name: 'email',
-    id: 'email',
-    placeholder: 'example@gmail.com',
+  const MockTextField: VFC = () => {
+    const {
+      register,
+      formState: { errors },
+    } = useForm();
+    const defaultProps: TextFieldProps = {
+      label: 'email',
+      register,
+      isError: !!errors,
+    };
+
+    return <TextField {...defaultProps} />;
   };
+
   test('should render ', () => {
-    render(<TextField {...defaultProps} />);
+    render(<MockTextField />);
     const textField = screen.getByRole('textbox');
     expect(textField).toBeInTheDocument();
   });
